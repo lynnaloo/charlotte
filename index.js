d874188b1feb02c86b7024a7e92aeaaca91cb73b
Linda Nichols <lynnaloo@gmail.com>
2015-02-26 20:14:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var RollingSpider = require('rolling-spider');
+var temporal = require('temporal');
+
+var charlotte = new RollingSpider();
+
+charlotte.connect(function() {
+  charlotte.setup(function() {
+
+
+    temporal.queue([
+      {
+        delay: 0,
+        task: function () {
+          charlotte.flatTrim();
+          charlotte.startPing();
+          charlotte.takeOff();
+        }
+      },
+      {
+        delay: 3000,
+        task: function () {
+          charlotte.forward();
+        }
+      },
+      {
+        delay: 500,
+        task: function () {
+          charlotte.land();
+        }
+      }]);
+  });
+});
